eb4cbbe6db133a8de15a71e8427339fec764ff9e
S550Stang <prabhuvardhanrao94@gmail.com>
2022-01-08 02:00:09 +0000

    new query

--- 1_queries/1_get_name_of_all_students_in_single_cohort.sql
+++ 1_queries/1_get_name_of_all_students_in_single_cohort.sql
@@ -1,11 +1,11 @@
-SELECT id, name 
-FROM students 
-WHERE cohort_id = 1
-ORDER BY name;
+-- SELECT id, name 
+-- FROM students 
+-- WHERE cohort_id = 1
+-- ORDER BY name;
 
-SELECT count(id)
-FROM students 
-WHERE cohort_id IN (1,2,3);
+-- SELECT count(id)
+-- FROM students 
+-- WHERE cohort_id IN (1,2,3);
 
 SELECT name, id, cohort_id
 FROM students
@@ -17,12 +17,12 @@ FROM students
 WHERE email NOT LIKE '%gmail.com' AND phone IS NULL
 ORDER BY id;  
 
-SELECT name, id, cohort_id
-FROM students
-WHERE end_date IS NULL
-ORDER BY cohort_id;  
+-- SELECT name, id, cohort_id
+-- FROM students
+-- WHERE end_date IS NULL
+-- ORDER BY cohort_id;  
 
-SELECT name, email, phone
-FROM students
-WHERE github IS NULL
-AND end_date IS NOT NULL;
+-- SELECT name, email, phone
+-- FROM students
+-- WHERE github IS NULL
+-- AND end_date IS NOT NULL;
